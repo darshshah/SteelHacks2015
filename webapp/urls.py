7966d5901186c1f3e459cc726898efcfe19b716b
Darsh Shah <darsh.shah11@gmail.com>
2015-03-28 02:06:20 -0400

    updated index.html form

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'webapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', blinker.views.home, name='home'),
+    url(r'^$',"blinker.views.home", name='home'),
 )
